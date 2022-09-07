70bc52c6e057b485100c02ed6b06e5a8468a62d4
Vegard1666 <i.lebedevqa@gmail.com>
2022-09-07 22:04:38 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/javacourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/javacourse/sandbox/MyFirstProgram.java
@@ -3,7 +3,24 @@ package ru.javacourse.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-System.out.println("Hello, world!");
+		hello("sdfhkhsdfk");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+	}
+	public static void hello(String somebody){
+		System.out.println("Hello, " + somebody + "!");
 	}
 
+	public static double area(double len){
+		return len * len;
+	}
+
+	public static double area(double a, double b){
+		return a * b;
+	}
 }
