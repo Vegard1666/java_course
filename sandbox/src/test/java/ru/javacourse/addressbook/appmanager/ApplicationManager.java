cb122834ba3dbfb8f0bca62fe11d7370e4f58022
Vegard1666 <i.lebedevqa@gmail.com>
2022-09-14 22:06:28 +0300

    Добавил ветвления (if-else) для выбора драйвера и заполнения формы

--- sandbox/src/test/java/ru/javacourse/addressbook/appmanager/ApplicationManager.java
+++ sandbox/src/test/java/ru/javacourse/addressbook/appmanager/ApplicationManager.java
@@ -2,20 +2,35 @@ package ru.javacourse.addressbook.appmanager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.time.Duration;
+import java.util.Objects;
 
 public class ApplicationManager {
+  private final Browser browser;
   WebDriver wd;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private JavascriptExecutor js;
 
+  public ApplicationManager(Browser browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    System.setProperty("webdriver.chrome.driver", "D:\\Git\\java_course\\sandbox\\chromedriver.exe");
-    wd = new ChromeDriver();
-   wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    if (Objects.equals(browser, Browser.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (Objects.equals(browser, Browser.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (Objects.equals(browser, Browser.EDGE)) {
+      wd = new EdgeDriver();
+    }
+//    System.setProperty("webdriver.chrome.driver", "D:\\Git\\java_course\\sandbox\\chromedriver.exe");
+    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     js = (JavascriptExecutor) wd;
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
@@ -36,6 +51,7 @@ public class ApplicationManager {
       return false;
     }
   }
+
   public GroupHelper getGroupHelper() {
     return groupHelper;
   }
